cbd0f852494b04c46d719e4fb92ea77af1e162dc
taher27 <34998783+taher27@users.noreply.github.com>
2024-05-03 11:10:22 +0000

    Unit test generated by RoostGPT
    Using AI Model gpt-4

--- src/test/java/br/masmangan/beecrowd/bee1007/ProductSteps.java
+++ src/test/java/br/masmangan/beecrowd/bee1007/ProductSteps.java
@@ -8,31 +8,38 @@ import static org.junit.Assert.assertEquals;
 
 public class ProductSteps {
 
-    private final Product product = new Product();
-    private int actual;
+	private final Product product = new Product();
 
-    @Given("first number is {int}")
-    public void first_number_is(int a) {
-        product.setA(a);
-    }
-    @Given("second number is {int}")
-    public void second_number_is(int b) {
-        product.setB(b);
-    }
-    @Given("third number is {int}")
-    public void third_number_is(int c) {
-        product.setC(c);
-    }
-    @Given("fourth number is {int}")
-    public void fourth_number_is(int c) {
-        product.setD(c);
-    }
-    @When("difference is calculated")
-    public void different_is_calculated() {
-        actual = product.getDifference();
-    }
-    @Then("result should be {int}")
-    public void result_should_be(int expected) {
-        assertEquals(expected, actual);
-    }
+	private int actual;
+
+	@Given("first number is {int}")
+	public void first_number_is(int a) {
+		product.setA(a);
+	}
+
+	@Given("second number is {int}")
+	public void second_number_is(int b) {
+		product.setB(b);
+	}
+
+	@Given("third number is {int}")
+	public void third_number_is(int c) {
+		product.setC(c);
+	}
+
+	@Given("fourth number is {int}")
+	public void fourth_number_is(int c) {
+		product.setD(c);
+	}
+
+	@When("difference is calculated")
+	public void different_is_calculated() {
+		actual = product.getDifference();
+	}
+
+	@Then("result should be {int}")
+	public void result_should_be(int expected) {
+		assertEquals(expected, actual);
+	}
+
 }
